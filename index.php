c56228d61161c18a9c37d302a92f87e63ae4c4f1
ZacNorris <erick_isaac98@hotmail.com>
2024-09-07 15:25:04 -0600

    Primera Clase

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">    
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device/width, initial-scale1.0">
+        <title>Mi Pagina Web</title>
+</head>
+<body>
+
+<header>
+<h1></h1>
+<p>Lorem ipsum dolor sit amet.</p>
+<a href="https:youtube.com">Ir a yutuf</a>
+</header>
+
+<section?>
+<h2>Titulo1</h2>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, soluta.</p>
+<div>
+
+<h3>Titulo2</h3>
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores quae fugit iure exercitationem sapiente id repellendus at beatae velit temporibus!</p>
+</div>
+</section>
+
+<footer>
+    <p>Todos los derechos reservados.</p>
+</footer>
+
+</body>
+</html>
