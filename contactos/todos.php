288a1d07b057a0c95cadba8544b54252f24bfb68
aldoaaa1 <48300527+aldoaaa1@users.noreply.github.com>
2024-09-07 15:24:14 -0600

    Primera Clase

---
+++ contactos/todos.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Todos Contactos</title>
+</head>
+<body>
+	<h1>Contactos</h1>
+	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione enim voluptatibus deleniti?</p>
+	<a href="http://localhost/Code/">Página Principal</a>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Mi Página Web</title>
+	<link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+	<header>
+		<h1>Titulo</h1>
+		<p class="header-texto">Lorem ipsum dolor sit amet.</p>
+		<a href="https://www.google.com.mx">Ir a google</a>
+	</header>
+
+	<section>
+		<h2 class="titulo1">Sección Principal</h2>
+
+		<div class="card">
+			<h3>Titulo 1</h3>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore ipsum veniam expedita.</p>
+		</div>
+
+		<!-- Yo quiero apuntar esto como comentario personal -->
+		<div class="card">
+			<h3>Titulo 2</h3>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore ipsum veniam expedita.</p>
+		</div>
+	</section>
+
+	<footer>
+		<p id="footer-text">Todos los derechos reservados.</p>
+	</footer>
+</body>
+</html>
