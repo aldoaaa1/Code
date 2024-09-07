5ae2953051fd58493d1dd613559b8b65d255f9fd
JCfields77 <jaasielcampos988@gmail.com>
2024-09-07 15:24:59 -0600

    Primera Clase

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi pagina web</title>
+</head>
+<body>
+    Hola mundo 
+</body>
+<h1>Titulo</h1>
+<p>Lorem ipsum dolor sit amet.</p>
+<a href="https://www.youtube.com/"></a>
+</header>
+
+/<section>
+    <h2>Seccion Princpal</h2>
+
+    <div>
+        <h3>Titulo 1</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque cupiditate sapiente corporis, quas in quam?</p>
+    </div>
+
+    <!-- Yo quiero apuntar esto como comnetario personal -->
+    <div>
+        <h3>Titulo 2</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio sint excepturi, soluta maiores voluptas sed!</p>
+    </div>
+
+</section>
+
+
+<h2>Titulo</h2>
+<h3>Titulo</h3>
+<h4>Titulo</h4>
+<h5>Titulo</h5>
+<h6>Titulo</h6>
+<p></p>Soy un parrafo
+<a href="https://www.youtube.com/">Ir a youtube</a>
+<div>
+    <h2>Clase de programación</h2>
+    <p>lorem20
+    </p>
+</div>
+</html>
